3d898e36b3891275f2b578e4276f3faa9d67c7a2
kahtava <kahtava@a895baa0-91a5-11dd-bcbf-bdb4bd03976f>
2008-10-05 19:45:03 +0000

    Added function (and test) to return an error when both the ListId or CustomerId are not provided.

--- trunk/AdamDotCom.Amazon/source/AdamDotCom.Amazon.Domain/AmazonFactory.cs
+++ trunk/AdamDotCom.Amazon/source/AdamDotCom.Amazon.Domain/AmazonFactory.cs
@@ -1,4 +1,5 @@
-﻿using AdamDotCom.Amazon.Domain;
+﻿using System.Collections.Generic;
+using AdamDotCom.Amazon.Domain;
 
 namespace AdamDotCom.Amazon.Domain
 {
@@ -6,26 +7,51 @@ namespace AdamDotCom.Amazon.Domain
     {
         private readonly ProductListMapper productListMapper;
         private readonly ReviewListMapper reviewListMapper;
+        private readonly IAmazonRequest amazonRequest;
 
         public AmazonFactory(IAmazonRequest amazonRequest)
         {
-            productListMapper = new ProductListMapper(amazonRequest);
+            this.amazonRequest = amazonRequest;
 
-            reviewListMapper = new ReviewListMapper(amazonRequest);
+            if (!string.IsNullOrEmpty(amazonRequest.ListId))
+            {
+                productListMapper = new ProductListMapper(amazonRequest);
+            }
+
+            if (!string.IsNullOrEmpty(amazonRequest.CustomerId))
+            {
+                reviewListMapper = new ReviewListMapper(amazonRequest);
+            }
         }
 
         public AmazonResponse GetResponse()
         {
             var amazonResponse = new AmazonResponse();
+            amazonResponse.Errors = new List<string>();
 
-            amazonResponse.Products = productListMapper.GetList();
-            amazonResponse.Errors = productListMapper.GetErrors();
-
-            amazonResponse.Reviews = reviewListMapper.GetReviewList();
-            
-            foreach (string error in reviewListMapper.GetErrors())
+            if(string.IsNullOrEmpty(amazonRequest.CustomerId) && string.IsNullOrEmpty(amazonRequest.ListId))
             {
-                amazonResponse.Errors.Add(error);
+                amazonResponse.Errors.Add("A CustomerId or a ListId must be specified.");
+            }
+
+            if (productListMapper != null)
+            {
+                amazonResponse.Products = productListMapper.GetList();
+
+                foreach (string error in productListMapper.GetErrors())
+                {
+                    amazonResponse.Errors.Add(error);
+                }
+            }
+
+            if (reviewListMapper != null)
+            {
+                amazonResponse.Reviews = reviewListMapper.GetReviewList();
+
+                foreach (string error in reviewListMapper.GetErrors())
+                {
+                    amazonResponse.Errors.Add(error);
+                }
             }
 
             return amazonResponse;
