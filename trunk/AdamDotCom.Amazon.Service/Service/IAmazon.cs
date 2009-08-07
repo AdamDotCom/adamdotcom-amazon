7915f1fc70cdc87b0866287d3f5db3f42c4592b6
kahtava <kahtava@a895baa0-91a5-11dd-bcbf-bdb4bd03976f>
2009-08-07 03:45:53 +0000

    Adding the base for my WCF service

---
+++ trunk/AdamDotCom.Amazon.Service/Service/IAmazon.cs
@@ -0,0 +1,13 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace AdamDotCom.Amazon.Service
+{
+    [ServiceContract]
+    public interface IAmazon
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "greet/{name}")]
+        string Greet(string name);
+    }
+}
---
+++ trunk/AdamDotCom.Amazon.Service/ServiceProxy/Amazon.cs
@@ -0,0 +1,15 @@
+﻿// The following line sets the default namespace for DataContract serialized typed to be ""
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+[assembly: ContractNamespace("", ClrNamespace = "AmazonService")]
+namespace AdamDotCom.Amazon.Service.Proxy
+{
+    public class AmazonService : ClientBase<IAmazon>, IAmazon
+    {
+        public string Greet(string name)
+        {
+            return base.Channel.Greet(name);
+        }
+    }
+}
