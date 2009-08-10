92771cc43e71a6fca01472afc6e7a458b50dd1aa
kahtava <kahtava@a895baa0-91a5-11dd-bcbf-bdb4bd03976f>
2009-08-10 19:05:35 +0000

    Resolved broken integration tests

--- trunk/AdamDotCom.Amazon/source/WebServiceTranslator/Interfaces/IReviewMapper.cs
+++ trunk/AdamDotCom.Amazon/source/WebServiceTranslator/Interfaces/IReviewMapper.cs
@@ -5,5 +5,7 @@ namespace AdamDotCom.Amazon.WebServiceTranslator.Interfaces
     public interface IReviewMapper
     {
         List<ReviewDTO> GetReviews();
+        
+        List<string> GetErrors();
     }
 }
