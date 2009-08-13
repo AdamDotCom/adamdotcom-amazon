9f09c48356f6a01938cdf0f7d4953c23c2e29ac0
kahtava <kahtava@a895baa0-91a5-11dd-bcbf-bdb4bd03976f>
2009-08-13 02:27:41 +0000

    Added rest exception stuff.

---
+++ trunk/AdamDotCom.Amazon.Service/Source/Service/Utilities/RestError.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace AdamDotCom.Amazon.Service.Utilities
+{
+    [DataContract]
+    public class RestError
+    {
+        public RestError()
+        {
+        }
+
+        public RestError(IDictionary dictionary, int httpStatusCode, int errorCode)
+        {
+            Errors = new Dictionary<string, string>();
+            foreach (DictionaryEntry item in dictionary)
+            {
+                Errors.Add((string) item.Key, (string) item.Value);
+            }
+
+            Description = string.Format("An error {0} occured with a HttpStatusCode {1}.", httpStatusCode, errorCode);
+        }
+
+        [DataMember] public IDictionary<string, string> Errors;
+
+        [DataMember] public string Description;
+    }
+}
---
+++ trunk/AdamDotCom.Amazon.Service/Source/Service/Utilities/WebHttpWithExceptions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace AdamDotCom.Amazon.Service.Utilities
+{
+    public class WebHttpWithExceptions : WebHttpBehavior
+    {
+        protected override void AddServerErrorHandlers(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
+        {
+            endpointDispatcher.ChannelDispatcher.ErrorHandlers.Clear();
+
+            endpointDispatcher.ChannelDispatcher.ErrorHandlers.Add(new ErrorHandler());
+        }
+
+        public class ErrorHandler : IErrorHandler
+        {
+            public bool HandleError(Exception error)
+            {
+                return true;
+            }
+
+            public void ProvideFault(Exception exception, MessageVersion version, ref Message fault)
+            {
+                var httpException = (HttpException) exception;
+
+                fault = Message.CreateMessage(version, null, new RestError(httpException.Data, httpException.GetHttpCode(), 10));
+
+                OutgoingWebResponseContext outResponse = WebOperationContext.Current.OutgoingResponse;
+                outResponse.StatusCode = (HttpStatusCode)httpException.GetHttpCode();
+            }
+        }
+    }
+}
