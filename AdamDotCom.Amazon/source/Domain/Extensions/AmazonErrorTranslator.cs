b3780d3d96ee4253c983937489237336aba400f9
kahtava <kahtava@a895baa0-91a5-11dd-bcbf-bdb4bd03976f>
2009-08-16 17:22:19 +0000

    Added additional error handling

---
+++ AdamDotCom.Amazon/source/Domain/Extensions/AmazonErrorTranslator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace AdamDotCom.Amazon.Domain.Extensions
+{
+    public static class AmazonErrorTranslator
+    {
+        public static KeyValuePair<string, string> Translate(this KeyValuePair<string, string> error)
+        {
+            if (error.Key.Contains("AWS.InvalidParameterValue"))
+            {
+                if (error.Value.Contains("CustomerId"))
+                {
+                    return new KeyValuePair<string, string>("CustomerId", error.Value);
+                }
+                if (error.Value.Contains("ListId"))
+                {
+                    return new KeyValuePair<string, string>("ListId", error.Value);
+                }
+            }
+            return error;
+        }
+    }
+}
--- AdamDotCom.Amazon/source/Domain/ProductListMapper.cs
+++ AdamDotCom.Amazon/source/Domain/ProductListMapper.cs
@@ -48,7 +48,7 @@ namespace AdamDotCom.Amazon.Domain
             }
             catch(Exception ex)
             {
-                errors.Add(new KeyValuePair<string, string>("Unknown", ex.Message));
+                errors.Add(new KeyValuePair<string, string>("ServiceError", ex.Message));
             }
         }
 
