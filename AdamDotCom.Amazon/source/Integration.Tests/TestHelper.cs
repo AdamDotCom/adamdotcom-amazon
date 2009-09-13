ae426dcf518748c728efd199b7f9e8e874d0d2d6
kahtava <kahtava@a895baa0-91a5-11dd-bcbf-bdb4bd03976f>
2009-09-13 17:06:46 +0000

    Added missing file

---
+++ AdamDotCom.Amazon/source/Integration.Tests/TestHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using AdamDotCom.Amazon.Domain;
+
+namespace AdamDotCom.Amazon.UnitTests
+{
+    public static class TestHelper
+    {
+        public static string AssociateTag = ConfigurationManager.AppSettings["AssociateTag"];
+        public static string AwsAccessKey = ConfigurationManager.AppSettings["AwsAccessKey"];
+        public static string SecretAccessKey = ConfigurationManager.AppSettings["SecretAccessKey"];
+        public static string CustomerId = "A2JM0EQJELFL69";
+        public static string ListId = "3JU6ASKNUS7B8";
+
+        public static AmazonRequest ValidAmazonRequest = new AmazonRequest
+                                                             {
+                                                                 AssociateTag = AssociateTag,
+                                                                 AccessKeyId = AwsAccessKey,
+                                                                 SecretAccessKey = SecretAccessKey,
+                                                                 CustomerId = CustomerId,
+                                                                 ListId = ListId
+                                                             };
+    }
+}
