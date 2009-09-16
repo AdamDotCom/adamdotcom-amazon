9d271d5696d8fbe3efc933f991942dfce571e955
kahtava <kahtava@a895baa0-91a5-11dd-bcbf-bdb4bd03976f>
2009-09-16 15:57:09 +0000

    Fixed nAnt testing script, added integration testing, fixed / hand coded the proxy to mirror the service changes.

--- trunk/AdamDotCom.Amazon.Service/Source/Integration.Tests/AmazonServiceTests.cs
+++ trunk/AdamDotCom.Amazon.Service/Source/Integration.Tests/AmazonServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using AdamDotCom.Amazon.Service.Proxy;
 
 namespace AdamDotCom.Amazon.Service.Integration.Tests
@@ -7,28 +8,68 @@ namespace AdamDotCom.Amazon.Service.Integration.Tests
     public class AmazonServiceTests
     {
         [Test]
+        //ToDo: Start WebDev.WebServer.exe before the tests are run in TestSetup then delete this test
+        public void SanityTest()
+        {
+            var amazonService = new AmazonService();
+
+            try
+            {
+                amazonService.DiscoverUsernameXml("Adam-Kahtava");
+            }
+            catch(Exception)
+            {
+                Assert.Fail("WebDev.WebServer.exe (cassini) probably isn't running, try starting up the ServiceHost project.");
+            }
+        }
+
+        [Test]
         public void ShouldVerifyProxyAndReturnReviews()
         {
             var amazonService = new AmazonService();
 
-            var response = amazonService.Reviews("A2JM0EQJELFL69");
+            var reviewsFromXmlRequest = amazonService.ReviewsByCustomerIdXml("A2JM0EQJELFL69");
 
-            Assert.IsNotNull(response);
-            Assert.IsNotNull(response.Reviews);
-            Assert.Greater(response.Reviews.Count, 1);
+            Assert.IsNotNull(reviewsFromXmlRequest);
+            Assert.IsNotNull(reviewsFromXmlRequest.Count);
+            Assert.Greater(reviewsFromXmlRequest.Count, 1);
+            
+            var reviewsFromJsonRequest = amazonService.ReviewsByCustomerIdJson("A2JM0EQJELFL69");
+
+            Assert.IsNotNull(reviewsFromJsonRequest);
+            Assert.AreEqual(reviewsFromXmlRequest.Count, reviewsFromJsonRequest.Count);
         }
 
-        [Test]
+        [Test, Ignore]
         public void ShouldVerifyProxyAndReturnWishlist()
         {
             var amazonService = new AmazonService();
 
-            var response = amazonService.Wishlist("3JU6ASKNUS7B8");
+            var wishlistFromXmlRequest = amazonService.WishlistByListIdXml("3JU6ASKNUS7B8");
 
-            Assert.IsNotNull(response);
-            Assert.IsNotNull(response.Products);
-            Assert.Greater(response.Products.Count, 1);
+            Assert.IsNotNull(wishlistFromXmlRequest);
+            Assert.Greater(wishlistFromXmlRequest.Count, 1);
+
+            var wishlistFromJsonRequest = amazonService.WishlistByListIdXml("3JU6ASKNUS7B8");
+
+            Assert.AreEqual(wishlistFromXmlRequest.Count, wishlistFromJsonRequest.Count);
         }
 
+        [Test]
+        public void ShouldVerifyProxyAndReturnProfile()
+        {
+            var amazonService = new AmazonService();
+
+            var profileFromXmlRequest = amazonService.DiscoverUsernameXml("Adam-Kahtava");
+
+            Assert.IsNotNull(profileFromXmlRequest);
+            Assert.IsFalse(string.IsNullOrEmpty(profileFromXmlRequest.CustomerId));
+            Assert.IsFalse(string.IsNullOrEmpty(profileFromXmlRequest.ListId));
+
+            var profileFromJsonRequest = amazonService.DiscoverUsernameJson("Adam-Kahtava");
+
+            Assert.AreEqual(profileFromXmlRequest.CustomerId, profileFromJsonRequest.CustomerId);
+            Assert.AreEqual(profileFromXmlRequest.ListId, profileFromJsonRequest.ListId);
+        }
     }
 }
